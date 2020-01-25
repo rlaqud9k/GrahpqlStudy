882d3c0b7163db3c9bf19015860b7dd10f6827d7
qudrhks <rlaqud9k@gmail.com>
2020-01-25 23:40:41 +0900

    buildSuccess

---
+++ graphql/resolvers.js
@@ -0,0 +1,14 @@
+const byeong = (name, age, gender) => {
+    name: name,
+    age: age,
+    gender: "gender"
+};
+
+const resolvers = {
+    Query : {
+        Person : (name, age, gender) =>
+    }
+
+}
+
+export default resolvers;
