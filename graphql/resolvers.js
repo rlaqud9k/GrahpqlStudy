29fc1818fd193fab8a61aeb1738e4158e77e8d67
qudrhks <rlaqud9k@gmail.com>
2020-01-29 01:07:59 +0900

    schemePractice

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,12 +1,34 @@
-const byeong = {
-    name: `rlaqudrhks`,
-    age: 42,
-    gender: "male"
-};
+const People = [
+    {
+        id: 1,
+        name: `rlaqudrhks`,
+        age: 2,
+        gender: "male"
+    },
+    {
+        id: 2,
+        name: `q`,
+        age: 5,
+        gender: "male"
+    },
+    {
+        id: 3,
+        name: `d`,
+        age: 213,
+        gender: "male"
+    },
+    {
+        id: 5,
+        name: `g`,
+        age: 44,
+        gender: "female"
+    }
+]
 
 const resolvers = {
-    Query : {
-        person : () => byeong
+    Query: {
+        people: () => People,
+        person: (i) => People[i]
     }
 
 }
